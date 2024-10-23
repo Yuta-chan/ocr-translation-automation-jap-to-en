c1131addbb31c1d4ec4e22b0612d0d58d7c49e74
Judith Urbina <78736744+Yuta-chan@users.noreply.github.com>
2024-10-23 18:16:16 +0900

    Update annotate_to_translate.py

--- annotate_to_translate.py
+++ annotate_to_translate.py
@@ -1,5 +1,5 @@
 from translate import *
-from annotate_text import *
+from annotate_pdf import *
 
 # Run the function to get the annotated text from the PDF
 annotated_text = sample_batch_annotate_files()
