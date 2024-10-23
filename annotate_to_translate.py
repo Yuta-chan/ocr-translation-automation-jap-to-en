8f3f7b3f2fc8053bbd1e3a3b054d4b47efecfbf4
Judith Urbina <78736744+Yuta-chan@users.noreply.github.com>
2024-10-23 18:33:07 +0900

    Update annotate_to_translate.py

--- annotate_to_translate.py
+++ annotate_to_translate.py
@@ -1,8 +1,19 @@
 from translate import *
 from annotate_pdf import *
 
+def give_pdf_file():
+    try:
+        pdf_file = str(input("Which PDF file would you like to extract text from? "))
+    except ValueError:
+        print("Please enter a valid PDF file name.")
+    else:
+        print(f"You have selected {pdf_file}")
+    return pdf_file
+# Get the PDF file name from the user
+pdf_file = give_pdf_file()
+
 # Run the function to get the annotated text from the PDF
-annotated_text = sample_batch_annotate_files()
+annotated_text = sample_batch_annotate_files(pdf_file)
 
 # Translate the extracted annotated text
 translated_text = sample_translate_text(annotated_text)
